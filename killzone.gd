a713716f23c5711d8a9df78c8736fa757f9f78bb
Manjot Singh <mithugunnu2006@gmail.com>
2024-12-06 10:31:46 +0530

    added player animations

--- killzone.gd
+++ killzone.gd
@@ -4,8 +4,10 @@ extends Area2D
 
 func _on_timer_timeout():
 	get_tree().reload_current_scene()# Replace with function body.
-
+	Engine.time_scale = 1
 
 func _on_body_entered(body):
+	body.get_node("CollisionShape2D").queue_free()
 	timer.start()
 	print("You died")
+	Engine.time_scale = 0.5
